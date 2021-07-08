16b7bba5ff75b35724b16bec6e5fc2d90cc0070d
Fabien Maussion <fabien.maussion@uibk.ac.at>
2021-07-08 19:38:05 +0200

    Better region check scripts, bug in l2

--- workflow/utils.py
+++ workflow/utils.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import numpy as np
 
 def mkdir(path, reset=False):
     """Checks if directory exists and if not, create one.
@@ -20,3 +21,21 @@ def mkdir(path, reset=False):
     except FileExistsError:
         pass
     return path
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between one point 
+    on the earth and an array of points (specified in decimal degrees)
+    """
+    
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    r = 6371000 # Radius of earth in meters
+    return c * r
